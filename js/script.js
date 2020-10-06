c1a9dacf30ee1bdd3d0b9a44a444353a7ef13d7d
Heather Ackerman <hla20115213@gmail.com>
2020-10-06 13:20:03 -0500

    Worked on local storage and created a few variables.

--- js/script.js
+++ js/script.js
@@ -1,41 +1,46 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
     var catAPIKey = "26f9bcd9-9356-4551-94c3-38ed60173dbb";
     var nytAPIKey = "7T7QfWEPRm3WkUcmcZFNLR4MYSjg0X1u";
     var catQueryURL = "https://api.thecatapi.com/v1/images/search?api_key=" + catAPIKey;
-<<<<<<< HEAD
     var userInput = document.querySelector("#search")
-
-    console.log(inputText);
-    var pastSearches = [];
+    var searchCount = document.querySelector("#searchCount")
+    var pastSearches = document.querySelector("#pastSearchesDisplay")
     
-function storeSearches() {
-    localStorage.setItem("pastSearches", JSON.stringify())
-}
+    console.log(inputText);
+    
 
-// 
-$("#run-search").on("click", function(event) {
 
-    var inputText = userInput.value.trim();
-    // if input field blank - return
-    if (inputText === "") {
-    return;
+    function getStoredInputs() {
+        var pastSearches = JSON.parse(localStorage.getItem("pastSearches"))
+        $("#pastSearchesDisplay").text(pastSearches)
     }
 
-    // add the search to our aray of searches)
-=======
-    var userInput = $("search").textcontent
->>>>>>> main
 
+    // 
+    $("#run-search").on("click", function (event) {
+
+        var inputText = userInput.value.trim();
+        // if input field blank - return
+        if (inputText === "") {
+            return;
+        } else {
+            localStorage.setItem("pastSearches", JSON.stringify(inputText))
+        }
+        getResponse();
+        // add the search to our aray of searches)
+    };
+
+    function getResponse() {
     var nytQueryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + userInput + "&api-key=" + nytAPIKey;
 
-            $.ajax({
-            url: nytQueryURL,
-            method: "GET"
-            })
-            .then(function(response) {
-                console.log(response)
-            })
+    $.ajax({
+        url: nytQueryURL,
+        method: "GET"
+    })
+        .then(function (response) {
+            console.log(response)
+        })
 
     $.ajax({
         url: catQueryURL,
@@ -43,12 +48,10 @@ $("#run-search").on("click", function(event) {
     })
         .then(function (response) {
             console.log(response)
-<<<<<<< HEAD
             console.log(response[0].url)
-          
+
             $("#catImg").attr("src", response[0].url)
         })
-=======
-        });
->>>>>>> main
+    }
+    getStoredInputs();
 });
