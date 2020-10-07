446a12d675269dc53babe2d123ae598d680ba5f0
Heather Ackerman <hla20115213@gmail.com>
2020-10-07 13:05:23 -0500

    Seperated local storage so the picture and article title URLs are stored to different locations.

--- js/script.js
+++ js/script.js
@@ -6,33 +6,22 @@ $(document).ready(function () {
     var userInput = document.querySelector("#search")
     var searchCount = document.querySelector("#searchCount")
     var pastSearches = document.querySelector("#pastSearchesDisplay")
-    
-    
-
+    var savedArticles = []
+    var savedCatPhotos = []
 
     function getStoredInputs() {
-        var pastSearches = JSON.parse(localStorage.getItem("pastSearches"))
-        $("#pastSearchesDisplay").text(pastSearches)
+        var pastArticleSearches = JSON.parse(localStorage.getItem("savedArticles"))
+        var pastSearchesComplete = pastArticleSearches + JSON.parse(localStorage.getItem("savedCatPhotos"))
+        $("#pastSearchesDisplay").text(pastSearchesComplete)
     }
 
-
-    // 
     $("#run-search").on("click", function (event) {
         event.preventDefault();
 
         var inputText = userInput.value.trim();
         console.log(inputText);
-        // if input field blank - return
-        if (inputText === "") {
-            return;
-        } else {
-            localStorage.setItem("pastSearches", JSON.stringify(inputText))
-        }
-        // getResponse();
-        // add the search to our aray of searches)
-    
-
-        // function getResponse() {
+        
+      
         var nytQueryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + inputText + "&api-key=" + nytAPIKey;
 
         $.ajax({
@@ -40,26 +29,35 @@ $(document).ready(function () {
         method: "GET"
         })
         .then(function (reply) {
-            console.log(reply)
-            console.log(reply.response.docs[3].headline.main)
-            console.log(reply.response.docs[3].lead_paragraph)
-            console.log(reply.response.docs[3].web_url)
+            // console.log(reply)
+            // console.log(reply.response.docs[3].headline.main)
+            // console.log(reply.response.docs[3].lead_paragraph)
+            // console.log(reply.response.docs[3].web_url)
 
             $("#card-title").text(reply.response.docs[3].headline.main)
             $("#lead-para").text(reply.response.docs[3].lead_paragraph)
             $("#web-url").attr("href", + reply.response.docs[3].web_url)
-        })
+            var responseArticle = reply.response.docs[3].web_url
+            
+            savedArticles.push({responseArticle});
+            localStorage.setItem("responseArticle", JSON.stringify(responseArticle))
+        });
 
         $.ajax({
         url: catQueryURL,
         method: "GET"
         })
         .then(function (response) {
-            console.log(response)
-            console.log(response[0].url)
+            // console.log(response)
+            // console.log(response[0].url)
 
             $("#catImg").attr("src", response[0].url)
+            var responseCatPhoto = response[0].url
+            
+            savedCatPhotos.push({responseCatPhoto});
+            localStorage.setItem("responseCatPhoto", JSON.stringify(responseCatPhoto))
         })
+        
     });
     getStoredInputs();
 });
