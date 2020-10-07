74b8b46db4162ff2d8518617bba3c51c1cb7ea89
B-Audette <b.audette015@gmail.com>
2020-10-07 15:54:50 -0500

    styled CSS and added cats only button feature

--- README.md
+++
@@ -1,2 +0,0 @@
-# Consolation_Cat_News_App
-A news app that feeds you articles along with a random photo of a cat to give you a serotonin boost (because the news is stressful).
--- js/script.js
+++ js/script.js
@@ -15,49 +15,77 @@ $(document).ready(function () {
         $("#pastSearchesDisplay").text(pastSearchesComplete)
     }
 
+
     $("#run-search").on("click", function (event) {
         event.preventDefault();
 
         var inputText = userInput.value.trim();
         console.log(inputText);
-        
-      
+
+
         var nytQueryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" + inputText + "&api-key=" + nytAPIKey;
 
         $.ajax({
-        url: nytQueryURL,
-        method: "GET"
+            url: nytQueryURL,
+            method: "GET"
         })
-        .then(function (reply) {
-            // console.log(reply)
-            // console.log(reply.response.docs[3].headline.main)
-            // console.log(reply.response.docs[3].lead_paragraph)
-            // console.log(reply.response.docs[3].web_url)
+            .then(function (reply) {
+                console.log(reply)
+                // console.log(reply.response.docs[3].headline.main)
+                // console.log(reply.response.docs[3].lead_paragraph)
+                // console.log(reply.response.docs[3].web_url)
+                var counter = 1;
 
-            $("#card-title").text(reply.response.docs[3].headline.main)
-            $("#lead-para").text(reply.response.docs[3].lead_paragraph)
-            $("#web-url").attr("href", + reply.response.docs[3].web_url)
-            var responseArticle = reply.response.docs[3].web_url
-            
-            savedArticles.push({responseArticle});
-            localStorage.setItem("responseArticle", JSON.stringify(responseArticle))
-        });
+                for (var i = 0; i < reply.response.docs.length; i++) {
 
+                    $("#card-title" + counter).text(reply.response.docs[i].headline.main)
+                    $("#lead-para" + counter).text(reply.response.docs[i].lead_paragraph)
+                    $("#web-url" + counter).attr("href", reply.response.docs[i].web_url)
+                    var responseArticle = reply.response.docs[i].web_url;
+                    counter++
+                    savedArticles.push({ responseArticle });
+                    callCats();
+                }
+                localStorage.setItem("responseArticle", JSON.stringify(responseArticle))
+            });
+
+        function callCats() {
+            var counter2 = 1;
+            for (var i = 1; i < 7; i++) {
+
+                $.ajax({
+                    url: catQueryURL,
+                    method: "GET"
+                })
+                    .then(function (response) {
+                        console.log(response)
+                        // console.log(response[0].url)
+
+                        $("#catImg" + counter2).attr("src", response[0].url)
+                        var responseCatPhoto = response[0].url
+                        counter2++
+                        savedCatPhotos.push({ responseCatPhoto });
+
+                        localStorage.setItem("responseCatPhoto", JSON.stringify(responseCatPhoto))
+                    })
+            }
+        }
+
+    });
+
+    $("#cats-only").on("click", function() {
         $.ajax({
-        url: catQueryURL,
-        method: "GET"
+            url: catQueryURL,
+            method: "GET"
         })
-        .then(function (response) {
-            // console.log(response)
-            // console.log(response[0].url)
+            .then(function (response) {
+                console.log(response)
+                // console.log(response[0].url)
 
-            $("#catImg").attr("src", response[0].url)
-            var responseCatPhoto = response[0].url
+                $("#cat-only").attr("src", response[0].url).attr("style", "width: 400px")
+            })
             
-            savedCatPhotos.push({responseCatPhoto});
-            localStorage.setItem("responseCatPhoto", JSON.stringify(responseCatPhoto))
-        })
-        
+                
     });
     getStoredInputs();
 });
